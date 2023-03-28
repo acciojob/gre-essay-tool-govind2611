4661d896fa79b0fdbc8a994c03468093e84cb917
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-28 23:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+ function countWords() {
+      let text = document.getElementById("evaluatedText").value.trim();
+      let wordCount = text.length > 0 ? text.match(/\S+/g).length : 0;
+      document.getElementById("wordCount").textContent = wordCount;
+    }
